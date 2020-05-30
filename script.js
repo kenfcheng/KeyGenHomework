c951bde9bc3e9aa15283f1084ea04c5b80acb9f5
Ken Cheng <kenfcheng@gmail.com>
2020-05-30 09:45:14 -0400

    function Array added

--- script.js
+++ script.js
@@ -17,11 +17,29 @@ generateBtn.addEventListener("click", writePassword);
 
 const values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefhijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
 
-let pasword = "";
+// Generating Password Prompts:
+function generatePassword() {
+  let passwordLength = prompt("Enter Number of Characters for Password");
 
-// Password char loop
-for(var i = 0; i <== complexity; i++){
-    password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.length))
+  const special = confirm("Do you want special characters?");
+
+  const numbers = confirm("Would you like numbers in password?");
+
+  const upperCase = confirm("Do you want upper cases?");
+
+  const lowerCase = confirm("Do you want lower cases?");
 }
 
+// Minimums
+const minimumCount = 0;
+
+const minimumSpecial = "";
+const minimumNumbers = "";
+const minimumUpperCase = "";
+const minimumLowerCase = "";
+
+const functionArray = {
+  getNumbers: function() {
+    return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+  }
 }
