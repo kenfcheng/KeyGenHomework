cef9aea254b94561a0321d7eec131a81c8b9176e
Ken Cheng <kenfcheng@gmail.com>
2020-06-02 18:02:29 -0400

    train wreck train wreck train wreck

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# SSHkeysHomework
+This assignment has been an up and down scenario.  I would have loved to have seen it come together, but I can't seem to get it to work.  I spent a bunch of hours on this and there were times I thought I was getting somewhere only to be stonewalled at certain points.
+
+A lot of my work in this is definitely influenced by stuff I read online and other resources I was able to find, which can be shown in my attempted design.  Orignally, I didn't plan on using slider bars and check boxes, and tried to follow the prompt pages like I think was expected.  However, after countless hours on that, it was apparent I wasn't getting anywhere with that, so I decided to try to use the same techniques I was self-learning from outside resources and tutorials.
+
+In a way I thought this would be comperable as it does do what the prompts asks, but in a more condensed way.
+
+I thought I was making good progress this way, but now it's at the end and something isn't quite working and I'm not sure what else to do.  
+
--- script.js
+++ script.js
@@ -2,34 +2,55 @@
 const characterAmountRange = document.getElementById('characterAmountRange')
 const characterAmountNumber = document.getElementById('characterAmountNumber')
 const includeUpperCasesEl = document.getElementById('includeUppercases')
-const includLowerCasesEl = document.getElementById('includeLowercases')
 const includeNumbersEl = document.getElementById
 ('includeNumbers')
 const includeSpecialCharactersEl = document.getElementById('includeSpecialCharacters')
 const form = document.getElementById('PasswordGenerator')
+const textArea = document.getElementById('PasswordDisplay')
 
 //Tells Array each individual char codes
 const Uppercases_Char_Codes = arrayFromLowtoHigh(65, 90)
 const Lowercases_Char_Codes = arrayFromLowtoHigh(97, 122)
 const Numbers_Char_Codes = arrayFromLowtoHigh(48, 57)
-const SpecialCharacters_Char_Codes = arrayFromLowtoHigh(33, 47).concat( arrayFromLowtoHigh(58, 64))
+const SpecialCharacters_Char_Codes = arrayFromLowtoHigh(33, 47).concat(arrayFromLowtoHigh(58, 64)).concat(
+    arrayFromLowtoHigh(91, 96)
+ ).concat(
+   arrayFromLowtoHigh(123, 126)
+) 
 
 characterAmountNumber.addEventListener('input', characterAmount)
 characterAmountRange.addEventListener('input', characterAmount)
  
-// Listener Prevents Reload also checks to see if elements are true.
-form.addEventListener('generate', event => { event.preventDefault()
+// Listener Prevents Page Reload 
+form.addEventListener('generate', e => {
+  e.preventDefault()
   const characterAmount = characterAmountNumber.value
+
   const includeUppercases = includeUppercasesEl.checked
-  const includeLowercases = includeLowercasesEl.checked
   const includeNumbers = includeNumbersEl.checked
   const includeSpecialCharacters = includeSpecialCharacterEl.checked
-  const password = generatePassword(characterAmount, includeUppercases, includeLowercases, includeNumbers, includeSpecialCharacters) { 
+  passwordDisplay.innerText = password
+  const password = generatePassword(characterAmount, includeUppercases, includeNumbers, includeSpecialCharacters)
 })
 
+
 //Where array begins from
-function generatePassword(characterAmount, includeUppercases, includeLowercases, includeNumbers, includeSpecialCharacters) {
+function generatePassword(characterAmount, includeUppercases, includeNumbers, includeSpecialCharacters) {
+  let charCodes = Lowercases_Char_Codes
+  if (includeUppercases) charCodes = charCodes.concat(Uppercases_Char_Codes)
+  if (includeNumbers) charCodes = charCodes.concat(Numbers_Char_Codes)
+  if (includeSpecialCharacters) charCodes = charCodes.concat(SpecialCharacters_Char_Codes)
+
+  //Give Random number between 0 and charAmount and gives code
+  const passwordCharacter = []
+  for (let i = 0; i < characterAmount; i++) {
+    const character = characterCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join('')
+//creates characters for password
   String.fromCharCode(65) //ASCII Code
+  console.log(Lowercases_Char_Codes)
 }
 //Indicates the array loop from low to high
 function arrayFromLowtoHigh(low, high) {
